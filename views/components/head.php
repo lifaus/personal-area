e4be6d19ca8e0270252ed3a4e0178b96077f6e78
Mark Willis <nouseruk@gmail.com>
2022-09-09 11:55:48 +0000

    Added class for connecting App\Services\Page components + Head and navbar component

---
+++ views/components/head.php
@@ -0,0 +1,4 @@
+<head>
+    <title>Home page</title>
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+</head>
--- views/pages/login.php
+++ views/pages/login.php
@@ -1,5 +1,14 @@
 <?php
-
-echo "Hello World";
-
+use App\Services\Page;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <?php Page::part('head'); ?>
+    <body>
+    <?php Page::part('navbar') ?>
+    <div class="container">
+
+    </div>
+    </body>
+</html>
